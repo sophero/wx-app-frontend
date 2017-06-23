1c07740fd66af0340dd8600c60e8c31796d3a8dc
Sophia Di <backwardsxmarathon@gmail.com>
2017-06-22 22:33:23 -0400

    Add sunrise and sunset time to page, converted to location time with timezone name displayed.

--- src/components/Geolocate.js
+++ src/components/Geolocate.js
@@ -4,7 +4,7 @@ import { geolocated } from 'react-geolocated';
 class Geolocate extends Component {
     constructor(props){
         super(props);
-    this.handleSetCoords = this.handleSetCoords.bind(this);
+        this.handleSetCoords = this.handleSetCoords.bind(this);
     }
 
     render() {
@@ -15,15 +15,18 @@ class Geolocate extends Component {
         } else {
             if (!this.props.isGeolocationEnabled) {
                 return(
-                    <div>Geolocation is not enabled.</div>
+                    <div>Geolocation might be disabled.</div>
                 );
             } else {
                 if (this.props.coords) {
+                    // this.handleSetMyLocCoords();
+                    let lat = Math.round(this.props.coords.latitude * 1000000) / 1000000;
+                    let lng = Math.round(this.props.coords.longitude * 1000000) / 1000000;
                     return(
                         <div>
                             <h2>Your location</h2>
-                            <div>Latitude: {this.props.coords.latitude}</div>
-                            <div>Longitude: {this.props.coords.longitude}</div>
+                            <div>Latitude: {lat}</div>
+                            <div>Longitude: {lng}</div>
                             <button onClick={this.handleSetCoords}>Use this location</button>
                         </div>
                     );
@@ -37,17 +40,17 @@ class Geolocate extends Component {
         }
     }
 
-  handleSetCoords() {
-      this.props.setCoords({
-          lat: this.props.coords.latitude,
-          lng: this.props.coords.longitude,
-          address: "Your location"
-      });
-  }
+    handleSetCoords() {
+        this.props.setCoords({
+            lat: this.props.coords.latitude,
+            lng: this.props.coords.longitude,
+            address: "Your location"
+        });
+    }
 
 }
 
 export default geolocated({
     positionOptions: { enableHighAccuracy: false },
-    userDecisionTimeout: 7000
+    userDecisionTimeout: 9000
 })(Geolocate);
